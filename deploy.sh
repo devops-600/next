d25f5c9619dee493e554a9440244af8f63023d44
Zhong Xun <kkzxak47@gmail.com>
2024-03-02 10:07:06 -0600

    refine deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+APPNAME="nextjs-app"
+echo "pm2 app name is $APPNAME"
 echo "copy new artifacts"
 cp -r ./* /www/next/
 cp -r ./.next /www/next/
 echo "restarting service"
-sudo -u lighthouse pm2 show nextjs-app
+sudo -u lighthouse pm2 show $APPNAME
 if [ $? -ne 0 ]
 then
-  cd /www/next/ && sudo -u lighthouse pm2 start "npm run start" --name nextjs-app
+  echo "$APPNAME does not exist, creating"
+  cd /www/next/ && sudo -u lighthouse pm2 start "npm run start" --name $APPNAME
 else
-  cd /www/next/ && sudo -u lighthouse pm2 stop nextjs-app
-  cd /www/next/ && sudo -u lighthouse pm2 start nextjs-app
+  echo "$APPNAME exists, restarting"
+  cd /www/next/ && sudo -u lighthouse pm2 stop $APPNAME
+  cd /www/next/ && sudo -u lighthouse pm2 start $APPNAME
 fi
 
 echo "service restarted"
