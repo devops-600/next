0b64e5345de28f00e984a1d9b5143e18c342724d
Zhong Xun <kkzxak47@gmail.com>
2024-03-01 22:24:00 -0600

    happy shell

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 cp -r ./* /www/next/
 cp -r ./.next /www/next/
-pm2 stop nextjs-app
-pm2 start nextjs-app
+cd /www/next/ && pm2 stop nextjs-app || true
+cd /www/next/ && pm2 start "npm run start" --name nextjs-app || true
+cd /www/next/ && pm2 start nextjs-app || true
