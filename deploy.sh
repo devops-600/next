2438d094d41f1c64b3a82fefa82bc44416435422
Zhong Xun <kkzxak47@gmail.com>
2024-03-02 10:00:47 -0600

    deploy.sh see if nextjs-app exist

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo "copy new artifacts"
 cp -r ./* /www/next/
 cp -r ./.next /www/next/
 echo "restarting service"
-cd /www/next/ && sudo -u lighthouse pm2 restart nextjs-app
+sudo -u lighthouse pm2 show nextjs-app
+if [ $? -ne 0 ]
+then
+  cd /www/next/ && sudo -u lighthouse pm2 start "npm run start" --name nextjs-app
+else
+  cd /www/next/ && sudo -u lighthouse pm2 stop nextjs-app
+  cd /www/next/ && sudo -u lighthouse pm2 start nextjs-app
+fi
+
 echo "service restarted"
 
