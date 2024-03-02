8b235cf852178ffbee435ccb4a73c599359e2990
Zhong Xun <kkzxak47@gmail.com>
2024-03-01 22:30:53 -0600

    sudo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+echo "copy new artifacts"
 cp -r ./* /www/next/
 cp -r ./.next /www/next/
-cd /www/next/ && pm2 stop nextjs-app || true
-cd /www/next/ && pm2 start "npm run start" --name nextjs-app || true
-cd /www/next/ && pm2 start nextjs-app || true
+echo "restarting service"
+cd /www/next/ && sudo pm2 restart nextjs-app
+echo "service restarted"
+
