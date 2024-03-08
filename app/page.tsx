cc8e8d55caae04bf76e11489218483efccc53cae
Zhong Xun <kkzxak47@gmail.com>
2024-03-08 14:07:55 -0600

    way to go

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ export default function Page() {
       <h1>App Router</h1>
       <p>hi pipeline zx</p>
       <p>2024-03-08</p>
+      <p>
+        way to go <del>no</del>
+        <strong>yes</strong>
+      </p>
     </>
   );
 }
