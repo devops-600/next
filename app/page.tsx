aea73803943b6b948c464ffbd72e059385c16d42
Zhong Xun <kkzxak47@gmail.com>
2024-04-05 16:12:25 -0600

    newlines for login

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Page() {
     <div className={styles.index}>
       <h1 className="text-3xl font-bold underline">Operations Platform</h1>
         <form className={styles.center}>
-            <input type="email" name="email" placeholder="Email" required />
-            <input type="password" name="password" placeholder="Password" required />
-            <button type="submit">Login</button>
+            <input type="email" name="email" placeholder="Email" required /><br/>
+            <input type="password" name="password" placeholder="Password" required /><br/>
+            <button type="submit">Login</button><br/>
         </form>
     </div>
   );
